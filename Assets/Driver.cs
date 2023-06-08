3b5221311f5d8f2ee9d7000039bf3486a3405ac6
Engincan Varan <engincanvaran@gmail.com>
2023-06-09 00:20:41 +0300

    Final Version

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steeringSpeed = 1, moveSpeed = 0.01f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float steeringSpeed = 200, moveSpeed = 15f, slowSpeed = 20f, fastSpeed = 30f;
 
     void Update()
     {
-        float steeringAmount = Input.GetAxis("Horizontal") * steeringSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steeringAmount = Input.GetAxis("Horizontal") * steeringSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steeringAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Debug.Log("Slowing down...");
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost") {
+            Debug.Log("Boosting...");
+            moveSpeed = fastSpeed;
+        }
     }
 }
