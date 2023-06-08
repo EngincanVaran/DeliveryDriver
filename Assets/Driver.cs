5391aeab2c4b3afe4c526e15ab77417b6fa2dffd
Engincan Varan <engincanvaran@gmail.com>
2023-06-08 22:37:24 +0300

    initial commits

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,11 +6,12 @@ public class Driver : MonoBehaviour
 {
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.01f);
+        transform.Translate(0.001f, 0, 0);
     }
 }
